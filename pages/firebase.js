aa2a2d1e921d9984992d22679c5e50521e8ff4c1
TheJhonni <davideconi91@gmail.com>
2022-02-09 10:17:37 +0100

    firebase.js

---
+++ pages/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTgwQPSvvParTfZkxhR812GTUDyPohxRk",
+  authDomain: "mywhatsapp34170.firebaseapp.com",
+  projectId: "mywhatsapp34170",
+  storageBucket: "mywhatsapp34170.appspot.com",
+  messagingSenderId: "829405233638",
+  appId: "1:829405233638:web:2fcd3030340ccbb9f31627",
+};
