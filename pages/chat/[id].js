4c67f26edabdb09c6537023008e8b515c814412b
TheJhonni <davideconi91@gmail.com>
2022-02-09 18:28:40 +0100

    sending data server side rendering

---
+++ pages/chat/[id].js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import { useAuthState } from "react-firebase-hooks/auth";
+import styled from "styled-components";
+import ChatScreen from "../../components/ChatScreen";
+import Sidebar from "../../components/Sidebar";
+import { auth, db } from "../../firebase";
+import getRecipientEmail from "../../utils/getRecepientEmail";
+
+function Chat({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  return (
+    <Container>
+      <Head>
+        <title>Chat with {getRecipientEmail(chat.users, user)}</title>
+      </Head>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen />
+      </ChatContainer>
+    </Container>
+  );
+}
+
+export default Chat;
+
+export async function getServerSideProps(context) {
+  const ref = db.collection("chats").doc(context.query.id);
+
+  //prep messages on the server!
+  const messageRef = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
+
+  const messages = messageRef.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+  //prep the chats!!
+  const chatRes = await ref.get("chat");
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none; /* FOR IE AND EDGE */
+  scrollbar-width: none; /* FIREFOX */
+`;
